8d8e21e9911bf29ab8291cf034bd46debc198bef
Scott Giese <scttgs0@gmail.com>
2022-07-15 22:24:49 -0500

    platform files

--- equates_game.asm
+++ equates_game.asm
@@ -1,12 +1,15 @@
 ;--------------------------------------
-;  MISCELLANEOUS MEMORY
+;  Miscellaneous Memory
 ;--------------------------------------
 
-PMAREA          = $0000                 ;P/M AREA
+PMAREA          = $0000                 ; p/m area
 MISSLS          = PMAREA+768
 PL0             = PMAREA+1024
 PL1             = PMAREA+1280
 PL2             = PMAREA+1536
 PL3             = PMAREA+1792
 
-DISP            = $0800                 ;DISPLAY
+DISP            = $B00000               ; display
+
+CharResX        = 40
+CharResY        = 30
--- equates_zeropage.asm
+++ equates_zeropage.asm
@@ -9,92 +9,92 @@
 
 LO              .byte ?
 HI              .byte ?
-PLOTX           .byte ?                 ; PLOT X VALUE
-PLOTY           .byte ?                 ; PLOT Y VALUE
-COLOR           .byte ?                 ; PLOT COLOR
-DRAWX           .byte ?                 ; DRAWTO X
-DRAWY           .byte ?                 ; DRAWTO Y
-ACCX            .byte ?                 ; X ACCUM.
-ACCY            .byte ?                 ; Y ACCUM.
-DELTAX          .byte ?                 ; DRAW WORK AREA
-DELTAY          .byte ?                 ; DRAW WORK AREA
-INCX            .byte ?                 ; DRAW X INCREMENT
-INCY            .byte ?                 ; DRAW Y INCREMENT
-COUNTR          .byte ?                 ; DRAWTO COUNTER
-ENDPT           .byte ?                 ; DRAW ENDPOINT
-HOLD            .byte ?                 ; WORK AREA
+PLOTX           .byte ?                 ; plot x value
+PLOTY           .byte ?                 ; plot y value
+COLOR           .byte ?                 ; plot color
+DRAWX           .byte ?                 ; drawto x
+DRAWY           .byte ?                 ; drawto y
+ACCX            .byte ?                 ; x accum.
+ACCY            .byte ?                 ; y accum.
+DELTAX          .byte ?                 ; draw work area
+DELTAY          .byte ?                 ; draw work area
+INCX            .byte ?                 ; draw x increment
+INCY            .byte ?                 ; draw y increment
+COUNTR          .byte ?                 ; drawto counter
+ENDPT           .byte ?                 ; draw endpoint
+HOLD            .byte ?                 ; work area
 XWORK           .byte ?
 YWORK           .byte ?
-GRID            .byte ?                 ; GRID # WORK
-OFFSET          .byte ?                 ; GRID OFFSET
-PMTIME          .byte ?                 ; PLAYER MOVE TIMER
-PLRGRD          .byte ?                 ; PLAYER GRID#
-PLRY            .byte ?                 ; PLAYER Y POS.
-GRIDNO          .byte ?                 ; GRID #
-LAST            .byte ?                 ; GRID...
-NEXT            .byte ?                 ; DIVIDE...
-STEP            .byte ?                 ; WORK...
-DEST            .byte ?                 ; AREAS
-VBXHLD          .byte ?                 ; X HOLD
-PFTIME          .byte ?                 ; PLAYER FIRE TIMER
-ENDVAL          .byte ?                 ; WORK AREA
-MISNUM          .byte ?                 ; MISSILE #
-PRFLIP          .byte ?                 ; PROJ. FLIP FLAG
-PRADVT          .byte ?                 ; PROJ. MOVE TIMER
-PRADV1          .byte ?                 ; PROJ. TIMER
-TIMER           .byte ?                 ; GENERAL TIMER
-INTRFG          .byte ?                 ; INTRO FLAG
-BCDLVL          .byte ?                 ; LEVEL #
-SHCOLR          .byte ?                 ; CHAR. COLOR
-SHOBYT          .byte ?                 ; CHAR. BYTE POS.
-YOFSET          .byte ?                 ; PLOT Y OFFSET
-OBTIM1          .byte ?                 ; OBJECT MOVE TIMER
-OBJNUM          .byte ?                 ; OBJECT #
-SHAPIX          .byte ?                 ; OBJ. SHAPE INDEX
-LENGTH          .byte ?                 ; OBJ. LENGTH
-XI              .byte ?                 ; OBJ. X INCREMENT
-YI              .byte ?                 ; OBJ. Y INCREMENT
-SHAPCT          .byte ?                 ; OBJ. SHAPE CNT.
-HLDGRD          .byte ?                 ; OBJ. GRID WORK
-PAUFLG          .byte ?                 ; PAUSE FLAG
-ZAP             .byte ?                 ; ZAP FLAG
-SAVEX           .byte ?                 ; WORK AREA
-SAVEY           .byte ?                 ; WORK AREA
-FLASHY          .byte ?                 ; OBJ. FLASH POS.
-FLTIME          .byte ?                 ; FLASH TIME
-SP1IX           .byte ?                 ; PLAYER...
-SP2IX           .byte ?                 ; SHAPE...
-SP3IX           .byte ?                 ; INDEX...
-SPIX            .byte ?                 ; AREAS
-PLTBYT          .byte ?                 ; PLYR. IMAGE BYTE
-PSCNT           .byte ?                 ; PLYR. SHAPE COUNT
-KILPLR          .byte ?                 ; KILL PLYR FLAG
-PAVAIL          .byte ?                 ; # PROJ. AVAILABLE
-TRANTM          .byte ?                 ; TRANSIENT TIMER
-DESTLO          .byte ?                 ; DESTINATION...
-DESTHI          .byte ?                 ; ADDRESS
-SHFLIP          .byte ?                 ; SHORT FLIP FLAG
-DESTNM          .byte ?                 ; SHORT PLYR #
-CPYST           .byte ?                 ; SHORT IMAGE START
-DMAC1           .byte ?                 ; DMA CTRL WORK
-GRAC1           .byte ?                 ; GRAPHICS CTRL WK.
-GRIDIX          .byte ?                 ; GRID INDEX
-LIVES           .byte ?                 ; LIVES LEFT
-GRDADJ          .byte ?                 ; GRID...
-GRDWK           .byte ?                 ; DRAW...
-GRDWK2          .byte ?                 ; WORK...
-GRDNUM          .byte ?                 ; AREAS
-OBJSPD          .byte ?                 ; OBJ. SPEED
-JOYPAD          .byte ?                 ; STICK/PADDLE
-CPYCNT          .byte ?                 ; SHORT COPY CNT.
-BONUS           .byte ?                 ; BONUS VALUE
-FIRSOU          .byte ?                 ; FIRE SOUND COUNT
-OBDSOU          .byte ?                 ; OBJ. DEATH SOUND
-MOVSOU          .byte ?                 ; PLYR MOVE SOUND
-PRYHLD          .fill 4                 ; PROJ. Y HOLDS
-SCORE           .fill 3                 ; SCORE
-SCOADD          .fill 3                 ; SCORE ADD VALUE
-MISCAD          .byte ?                 ; MISC. SCORE ADD
-NUMOBJ          .fill 5                 ; OBJECTS LEFT
-DIFF            .byte ?                 ; DIFFICULTY ADJUST
-OBJPRS          .fill 6                 ; OBJ PRESENT FLAGS
+GRID            .byte ?                 ; grid # work
+OFFSET          .byte ?                 ; grid offset
+PMTIME          .byte ?                 ; player move timer
+PLRGRD          .byte ?                 ; player grid#
+PLRY            .byte ?                 ; player y pos.
+GRIDNO          .byte ?                 ; grid #
+LAST            .byte ?                 ; grid...
+NEXT            .byte ?                 ; divide...
+STEP            .byte ?                 ; work...
+DEST            .byte ?                 ; areas
+VBXHLD          .byte ?                 ; x hold
+PFTIME          .byte ?                 ; player fire timer
+ENDVAL          .byte ?                 ; work area
+MISNUM          .byte ?                 ; missile #
+PRFLIP          .byte ?                 ; proj. flip flag
+PRADVT          .byte ?                 ; proj. move timer
+PRADV1          .byte ?                 ; proj. timer
+TIMER           .byte ?                 ; general timer
+INTRFG          .byte ?                 ; intro flag
+BCDLVL          .byte ?                 ; level #
+SHCOLR          .byte ?                 ; char. color
+SHOBYT          .byte ?                 ; char. byte pos.
+YOFSET          .byte ?                 ; plot y offset
+OBTIM1          .byte ?                 ; object move timer
+OBJNUM          .byte ?                 ; object #
+SHAPIX          .byte ?                 ; obj. shape index
+LENGTH          .byte ?                 ; obj. length
+XI              .byte ?                 ; obj. x increment
+YI              .byte ?                 ; obj. y increment
+SHAPCT          .byte ?                 ; obj. shape cnt.
+HLDGRD          .byte ?                 ; obj. grid work
+PAUFLG          .byte ?                 ; pause flag
+ZAP             .byte ?                 ; zap flag
+SAVEX           .byte ?                 ; work area
+SAVEY           .byte ?                 ; work area
+FLASHY          .byte ?                 ; obj. flash pos.
+FLTIME          .byte ?                 ; flash time
+SP1IX           .byte ?                 ; player...
+SP2IX           .byte ?                 ; shape...
+SP3IX           .byte ?                 ; index...
+SPIX            .byte ?                 ; areas
+PLTBYT          .byte ?                 ; plyr. image byte
+PSCNT           .byte ?                 ; plyr. shape count
+KILPLR          .byte ?                 ; kill plyr flag
+PAVAIL          .byte ?                 ; # proj. available
+TRANTM          .byte ?                 ; transient timer
+DESTLO          .byte ?                 ; destination...
+DESTHI          .byte ?                 ; address
+SHFLIP          .byte ?                 ; short flip flag
+DESTNM          .byte ?                 ; short plyr #
+CPYST           .byte ?                 ; short image start
+DMAC1           .byte ?                 ; dma ctrl work
+GRAC1           .byte ?                 ; graphics ctrl wk.
+GRIDIX          .byte ?                 ; grid index
+LIVES           .byte ?                 ; lives left
+GRDADJ          .byte ?                 ; grid...
+GRDWK           .byte ?                 ; draw...
+GRDWK2          .byte ?                 ; work...
+GRDNUM          .byte ?                 ; areas
+OBJSPD          .byte ?                 ; obj. speed
+JOYPAD          .byte ?                 ; stick/paddle
+CPYCNT          .byte ?                 ; short copy cnt.
+BONUS           .byte ?                 ; bonus value
+FIRSOU          .byte ?                 ; fire sound count
+OBDSOU          .byte ?                 ; obj. death sound
+MOVSOU          .byte ?                 ; plyr move sound
+PRYHLD          .fill 4                 ; proj. y holds
+SCORE           .fill 3                 ; score
+SCOADD          .fill 3                 ; score add value
+MISCAD          .byte ?                 ; misc. score add
+NUMOBJ          .fill 5                 ; objects left
+DIFF            .byte ?                 ; difficulty adjust
+OBJPRS          .fill 6                 ; obj present flags
