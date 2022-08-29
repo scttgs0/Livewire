7afa85d8dd7c0f64a42e7ff637c3acb30b0e0829
Scott Giese <scttgs0@gmail.com>
2022-08-28 20:59:49 -0500

    refactor Rendering

--- render.asm
+++ render.asm
@@ -1,109 +1,118 @@
 ;======================================
-; CLEAR Player-MISSILES
+; Clear Sprites
 ;======================================
-PMCLR           .proc
-;                lda #0                  ; put 255
-;                tax                     ; zeros in
-;PMCLP           sta MISSLS,X            ; each p/m
-;                sta PL0,X               ; area
-;                sta PL1,X
-;                sta PL2,X
-;                sta PL3,X
-;                dex
-;                bne PMCLP
+ClearSprites    .proc
+                .m16
 
+                lda #0
+                sta SP00_X_POS
+                sta SP00_Y_POS
+
+                .m8
                 rts
                 .endproc
 
 
 ;======================================
-; CLEAR SCREEN
+; Clear Screen
 ;======================================
-CLRSC           .proc
-                lda #>Playfield         ; initial
-                sta HI                  ; display
-                lda #<Playfield         ; address
-                sta LO                  ; work area
-                ldx #20                 ; clear 20 groups
-CLRSC2          ldy #0                  ; of 256 bytes
-                tya                     ; (5120 bytes)
-CLRSC3          sta (LO),Y
+ClearPlayfield  .proc
+                lda #>Playfield         ; initial display address work area
+                sta HI
+                lda #<Playfield
+                sta LO
+
+                ldx #20                 ; clear 20 groups of 256 bytes (5120 bytes)
+_next1          ldy #0                  ; note: playfield is 32-bytes * 160 lines = 5120 bytes
+                tya
+_next2          sta (LO),Y
                 dey
-                bne CLRSC3
+                bne _next2
 
                 dex
-                bne CLRSC4
+                bne _1
 
                 lda #TRUE
                 sta isDirtyPlayfield
                 rts
 
-CLRSC4          inc HI
-                jmp CLRSC2
+_1              inc HI
+                bra _next1
 
                 .endproc
 
 
 ;======================================
-; GR. 7+ PLOTTER ROUTINE
+; GR. 14 PLOTTER ROUTINE
 ;--------------------------------------
 ; (SEE A.N.A.L.O.G. #11)
 ;======================================
-PLOTCL          .proc
+PlotPoint       .proc
                 lda PLOTY               ; mult. y by 32:
                 asl A
                 sta LO
+
                 lda #0
                 rol A
                 sta HI                  ; *2
+
                 asl LO
                 rol HI                  ; *4
+
                 asl LO
                 rol HI                  ; *8
+
                 asl LO
                 rol HI                  ; *16
+
                 asl LO
                 rol HI                  ; *32
-                lda #<Playfield         ; add the display
-                clc                     ; address to get
-                adc LO                  ; the actual
-                sta LO                  ; address of the
-                lda #>Playfield         ; byte that will
-                adc HI                  ; be altered for
-                sta HI                  ; the plot.
-                lda PLOTX               ; mask plotx for
-                and #3                  ; plot index,
-                tax                     ; place in x.
-                lda PLOTX               ; get plotx and
-                lsr A                   ; divide
-                lsr A                   ; by 4,
+
+                lda #<Playfield         ; add the display address to get the actual
+                clc                     ; address of the byte that will be altered for
+                adc LO                  ; the plot.
+                sta LO
+                lda #>Playfield
+                adc HI
+                sta HI
+
+                lda PLOTX               ; mask plotx for plot index, place in x
+                and #3
+                tax
+
+                lda PLOTX               ; get plotx and divide by 4 (4 pixels/byte)
+                lsr A                   ; /4
+                lsr A
                 sta YOFSET
+
                 tay
                 lda (LO),Y
-                and BMASK2,X
-                cmp COLR1,X
-                beq PABORT
+                and BMASK2,X            ; isolate the 2-bits (4-color pixel)
+                cmp COLR1,X             ; skip if already color 1
+                beq _XIT
 
                 ldy COLOR               ; get color
-                lda BMASK2,X            ; and mask off
-                and COLORS,Y            ; pixel position
-                sta HOLD                ; save it,
-                lda BMASK1,X            ; mask off pixel
-                ldy YOFSET              ; of the address
-                and (LO),Y              ; to be altered
-                ora HOLD                ; set the plot
-                sta (LO),Y              ; bits and store!
+                lda BMASK2,X            ; mask off pixel position
+                and COLORS,Y
+                sta HOLD                ; save it
 
-PABORT          lda #TRUE
+                lda BMASK1,X            ; mask off pixel of the address to be altered
+                ldy YOFSET
+                and (LO),Y              ; set the plot bits
+                ora HOLD
+                sta (LO),Y              ; and store!
+
+                lda #TRUE
                 sta isDirtyPlayfield
-                rts
+
+_XIT            rts
                 .endproc
 
 ;--------------------------------------
 
-;
+; ----------------
 ; PLOT MASK TABLES
-;
+; ----------------
 
 COLORS          .byte $00,$55,$AA,$FF
 BMASK1          .byte $3F,$CF,$F3,$FC
@@ -112,104 +121,126 @@ COLR1           .byte $40,$10,$04,$01
 
 
 ;======================================
-; DRAW HANDLER
+; Draw Line
 ;======================================
-DRAW            .proc
+DrawLine        .proc
                 lda DRAWY
-                cmp PLOTY               ; is drawy>ploty?
-                bcc YMINUS              ;   no!
+                cmp PLOTY               ; is drawy>=ploty?
+                bcc _yMinus             ;   no!
 
+;   DRAWY >= PLOTY... calculate delta-Y
                 sec                     ; subtract
                 sbc PLOTY               ; ploty from drawy
-                sta DELTAY              ; and save difference.
-                lda #1                  ; y increment
-                sta INCY                ; = 1 (down)
-                bne XVEC                ; branch!
+                sta DELTAY              ; and save difference
 
-YMINUS          lda PLOTY               ; subtract
-                sec                     ; drawy
-                sbc DRAWY               ; from ploty
-                sta DELTAY              ; and save difference.
-                lda #255                ; y increment
-                sta INCY                ; = -1 (up)
-XVEC            lda DRAWX               ; is drawx
-                cmp PLOTX               ; > plotx?
-                bcc XMINUS              ;   no!
+;   we will draw from top toward bottom
+                lda #1                  ; y increment = 1 (down)
+                sta INCY
+                bra _xVec
 
+;   DRAWY < PLOTY... calculate delta-Y
+_yMinus         lda PLOTY
+                sec                     ; subtract
+                sbc DRAWY               ; drawy from ploty
+                sta DELTAY              ; and save difference
+
+;   we will draw from bottom toward top
+                lda #255                ; y increment = -1 (up)
+                sta INCY
+
+_xVec           lda DRAWX
+                cmp PLOTX               ; is drawx > plotx?
+                bcc _xMinus             ;   no!
+
+;   DRAWX >= PLOTX... calculate delta-X
                 sec                     ; subtract
                 sbc PLOTX               ; plotx from drawx
-                sta DELTAX              ; and save difference.
-                lda #1                  ; x increment
-                sta INCX                ; is 1 (right)
-                bne VECSET              ; branch!
+                sta DELTAX              ; and save difference
 
-XMINUS          lda PLOTX               ; subtract
-                sec                     ; drawx from
-                sbc DRAWX               ; plotx
-                sta DELTAX              ; and save difference.
-                lda #255                ; x increment
-                sta INCX                ; is -1 (left)
-VECSET          lda #0                  ; zero out:
+;   we will draw from left toward right
+                lda #1                  ; x increment = 1 (right)
+                sta INCX
+                bra _setVec
+
+; DRAWX < PLOTX... calculate detla-Y
+_xMinus         lda PLOTX
+                sec                     ; subtract
+                sbc DRAWX               ; drawx from plotx
+                sta DELTAX              ; and save difference
+
+;   we will draw from right toward left
+                lda #255                ; x increment = -1 (left)
+                sta INCX
+
+_setVec         lda #0                  ; zero out:
                 sta ACCY                ; y accumulator
                 sta ACCX                ; x accumulator
-                lda DELTAX              ; is deltax>
-                cmp DELTAY              ; deltay?
-                bcc YMAX                ;   no!
 
-                sta COUNTR              ; save deltax
-                sta ENDPT               ; in countr, endpt.
-                lsr A                   ; divide by 2 and
+                lda DELTAX
+                cmp DELTAY              ; is deltax >= deltay?
+                bcc _yBigger            ;   no!
+
+;   delta-x is bigger
+                sta COUNTR              ; save deltax in counter
+                sta ENDPT               ; and endpoint
+
+                lsr A                   ; /2
                 sta ACCY                ; store in y accum.
-                jmp DRAWGO              ; start draw
+                bra _doDraw             ; start draw
 
-YMAX            lda DELTAY              ; deltay larger,
-                sta COUNTR              ; store it in
-                sta ENDPT               ; countr, endpt.
-                lsr A                   ; divide by 2 and
+;   delta-y is bigger
+_yBigger        lda DELTAY
+                sta COUNTR              ; store it in counter
+                sta ENDPT               ; and endpoint
+
+                lsr A                   ; /2
                 sta ACCX                ; store in x accum.
 
-; -----------------------
-; NOW WE START THE ACTUAL
-; DRAWTO FUNCTION!
-; -----------------------
+; ----------------------------------------
+; Now we start the actual DRAWTO function!
+; ----------------------------------------
 
-DRAWGO          lda COUNTR              ; if countr=0...
-                beq DRWEND              ; no draw!
+_doDraw         lda COUNTR              ; if countr=0...
+                beq _XIT                ;   no draw!
 
-BEGIN           lda ACCY                ; add deltay
+_next1          lda ACCY                ; add deltay
                 clc                     ; to y accumulator
                 adc DELTAY
                 sta ACCY
                 cmp ENDPT               ; at endpoint yet?
-                bcc BEGIN2              ;   no, go do x.
+                bcc _doX                ;   no, go do x.
 
                 lda ACCY                ; subtract endpt
                 sec                     ; from y accumulator
                 sbc ENDPT
                 sta ACCY
+
                 lda PLOTY               ; and increment
                 clc                     ; the y position!
                 adc INCY
                 sta PLOTY
-BEGIN2          lda ACCX                ; add deltax to
+
+_doX            lda ACCX                ; add deltax to
                 clc                     ; x accumulator
                 adc DELTAX
                 sta ACCX
                 cmp ENDPT               ; at endpoint yet?
-                bcc PLOTIT              ;   no, go plot.
+                bcc _plotIt             ;   no, go plot.
 
                 lda ACCX                ; subtract endpt
                 sec                     ; from x accumulator
                 sbc ENDPT
                 sta ACCX
+
                 lda PLOTX               ; and increment
                 clc                     ; plot x
                 adc INCX
                 sta PLOTX
-PLOTIT          jsr PLOTCL              ; plot the point!
+
+_plotIt         jsr PlotPoint           ; plot the point!
 
                 dec COUNTR              ; more to draw?
-                bne BEGIN               ;   yes!
+                bne _next1              ;   yes!
 
-DRWEND          rts
+_XIT            rts
                 .endproc
