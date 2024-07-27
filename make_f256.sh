d3e791c76358a61b036fc1b2c78c628671d30984
scttgs0 <scttgs0@gmail.com>
2024-07-26 22:40:26 -0500

    updaate build script for PGX and BIN

--- make_f256.sh
+++ make_f256.sh
@@ -6,7 +6,18 @@ mkdir -p obj/
 64tass  --m65c02 \
         --flat \
         --nostart \
+        -D PGX=1 \
         -o obj/livewire.pgx \
         --list=obj/livewire.lst \
         --labels=obj/livewire.lbl \
+        livewire.asm
+
+
+64tass  --m65c02 \
+        --flat \
+        --nostart \
+        -D PGX=0 \
+        -o obj/livewireB.bin \
+        --list=obj/livewireB.lst \
+        --labels=obj/livewireB.lbl \
         livewire.asm
